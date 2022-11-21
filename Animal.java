a514e5e27b21115bf394bcfbd9e350158202b5b5
Niko <wrnkt1@gmail.com>
2022-11-20 22:39:47 -0500

    Frame system

--- Animal.java
+++ Animal.java
@@ -1,5 +1,26 @@
 public class Animal implements Exhibitable<Exhibit>
 {
+    public void announce(Exhibit e)
+    {
+        System.out.println();
+    }
+
+    public void releaseInto(Exhibit e)
+    {
+        System.out.println();
+    }
+
+    public void removeFrom(Exhibit e)
+    {
+        System.out.println();
+    }
+
+    public void exhibitMe(Exhibit e)
+    {
+        releaseInto(e);
+        removeFrom(e);
+    }
+
     public static void main(String[] args)
     {
     }
--- Exhibitable.java
+++ Exhibitable.java
@@ -1,12 +1,16 @@
+import java.time.Duration;
+
 public abstract interface Exhibitable<T>
 {
     public abstract void announce(T e);
-    public default void exhibitMe(T e)
+    
+    public abstract void releaseInto(T e);
+    public abstract void removeFrom(T e);
+
+    public default void showAnimal(T e, Duration duration)
     {
         releaseInto(e);
         removeFrom(e);
     }
-    public abstract void releaseInto(T e);
-    public abstract void removeFrom(T e);
 
 }
