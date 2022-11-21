946dd6a2858998893d0b49bdb5847fc952e05e48
Niko <wrnkt1@gmail.com>
2022-11-21 00:49:12 -0500

    Add fields, getters, setters to Animal

--- Animal.java
+++ Animal.java
@@ -1,7 +1,50 @@
+import java.time.LocalDateTime;
+
 public class Animal implements Exhibitable<Exhibit>
 {
 
+    public static int numberAnimals;
+    private int animalID;
+
+    private String name;
+
+    private LocalDateTime birthday;
+
     private boolean beingShown = false;
+
+    public Animal()
+    {
+        beBorn();
+        setName("unnamed");
+    }
+
+    public Animal(String name)
+    {
+        beBorn();
+        setName(name);
+    }
+
+    public final void beBorn()
+    {
+        numberAnimals++;
+        this.animalID = numberAnimals;
+        this.birthday = LocalDateTime.now();
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    private void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String toString()
+    {
+        return String.format("Animal ID #%d\n, Name: %s", animalID, name);
+    }
 
     public void releaseInto(Exhibit exhibit)
     {
