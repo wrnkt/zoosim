6b5ec6414f208998159679b6f47e20b110f633da
Niko <wrnkt1@gmail.com>
2022-11-28 11:03:28 -0500

    Simplify/generalize interface

--- Housable.java
+++ Housable.java
@@ -6,10 +6,4 @@ public abstract interface Housable<T>
     public abstract void releaseInto(T e);
     public abstract void removeFrom(T e);
 
-    public default void showAnimal(T e, Duration duration)
-    {
-        releaseInto(e);
-        removeFrom(e);
-    }
-
 }
